c615966e7da1af79b639c1943b503a7352d055eb
KamranJabiyev <kamran.jabiyev@code.edu.az>
2023-09-07 16:18:22 +0400

    Created Category Dto and Validator

---
+++ src/Core/Fiorello.Application/Validators/CategoryValidators/CategoryCrateDtoValidator.cs
@@ -0,0 +1,16 @@
+﻿using Fiorello.Application.DTOs.CategoryDtos;
+using FluentValidation;
+
+namespace Fiorello.Application.Validators.CategoryValidators;
+
+public class CategoryCrateDtoValidator: AbstractValidator<CategoryCreateDto>
+{
+    public CategoryCrateDtoValidator()
+    {
+        RuleFor(x => x.Name)
+            .NotEmpty().
+            MaximumLength(100);
+        RuleFor(x=>x.Description).
+            MaximumLength(500);
+    }
+}
--- src/Presentation/Fiorello.API/Program.cs
+++ src/Presentation/Fiorello.API/Program.cs
@@ -1,4 +1,6 @@
+using Fiorello.Application.Validators.CategoryValidators;
 using Fiorello.Persistence.Contexts;
+using FluentValidation;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -12,6 +14,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+//Fluent Validation
+builder.Services.AddValidatorsFromAssemblyContaining<CategoryCrateDtoValidator>();
 
 var app = builder.Build();
 
